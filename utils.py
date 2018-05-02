5277c1030bd249cad7a7a00512ced5b159005c53
Joan Massich <sik@visor.udg.edu>
2018-05-02 16:59:15 +0200

    remove unused import

--- utils.py
+++ utils.py
@@ -1,12 +1,14 @@
 from config import mne_data_path
 from mne import read_evokeds
-from mne.io import RawArray
+
 
 def _get_raw_path(subject_id):
     return '{}/{}/{}_RAW.fif'.format(mne_data_path, subject_id, subject_id)
 
+
 def _get_maxfilter_path(subject_id):
     return '{}/{}/{}_MAXfilter.fif'.format(mne_data_path, subject_id, subject_id)
+
 
 def get_data(subject_id=None, maxfilter=True):
     if subject_id is None:
@@ -17,4 +19,3 @@ def get_data(subject_id=None, maxfilter=True):
     else:
         evokeds = read_evokeds(_get_raw_path(subject_id))
     return evokeds[0]
-
