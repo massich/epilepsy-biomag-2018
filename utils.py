36bad91fa8ab4cf4b1eea9d03a88dd8492c97b4a
Joan Massich <sik@visor.udg.edu>
2018-05-02 14:07:28 +0200

    wip

--- utils.py
+++ utils.py
@@ -5,11 +5,19 @@ from mne.io import RawArray
 def _get_raw_path(subject_id):
     return '{}/{}/{}_RAW.fif'.format(mne_data_path, subject_id, subject_id)
 
+def _get_maxfilter_path(subject_id):
+    return '{}/{}/{}_MAXfilter-ave.fif'.format(mne_data_path, subject_id, subject_id)
 
 def get_raw_data(subject_id=None):
     if subject_id is None:
         raise RuntimeError('subject is a required parameter')
 
     evokeds = read_evokeds(_get_raw_path(subject_id))
-    return RawArray(evokeds[0].data, evokeds[0].info)
+    return evokeds[0]
 
+def get_maxfilter_data(subject_id=None):
+    if subject_id is None:
+        raise RuntimeError('subject is a required parameter')
+
+    evokeds = read_evokeds(_get_maxfilter_path(subject_id))
+    return evokeds[0]
