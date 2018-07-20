adfe38d413719b733369435678b4238f093a59f0
Joan Massich <sik@visor.udg.edu>
2018-07-20 14:44:11 +0200

    Update sik's config to use dropbox

--- config.py
+++ config.py
@@ -5,8 +5,10 @@ host = uname()[1]
 user = os.environ['USER']
 
 if host == 'toothless':
-    subjects_dir = '/home/sik/retreat/Biomag2018/freesurfer'
-    mne_data_path = '/home/sik/retreat/Biomag2018/original_data'
+    subjects_dir = '/home/sik/Dropbox/Biomag2018/freesurfer'
+    mne_data_path = '/home/sik/Dropbox/Biomag2018/original_data'
+    # subjects_dir = '/home/sik/retreat/Biomag2018/freesurfer'
+    # mne_data_path = '/home/sik/retreat/Biomag2018/original_data'
 elif user == 'alex':
     subjects_dir = '/Users/alex/Dropbox/Biomag2018/freesurfer'
     mne_data_path = '/Users/alex/Dropbox/Biomag2018/original_data'
