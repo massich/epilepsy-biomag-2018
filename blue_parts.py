aa92fa1582bbd58fb908146287fce077b1208e95
Joan Massich <sik@visor.udg.edu>
2018-07-20 17:56:44 +0200

    Iter in subject 245

--- blue_parts.py
+++ blue_parts.py
@@ -10,7 +10,7 @@ import utils
 
 plt.close('all')
 
-subject = subject_ids[0]
+subject = subject_ids[1]
 fig_folder = os.path.join(mne_data_path, '..', 'figures', subject)
 evoked_clean_fname = os.path.join(mne_data_path, subject, '%s-ave.fif' % subject)
 trans_fname = os.path.join(mne_data_path, subject, "%s-trans.fif" % subject)
